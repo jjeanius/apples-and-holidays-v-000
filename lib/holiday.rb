aba1f5d758be45a51630bb9cf5d03515c87318a5
Judy McGoldrick <jjeanius@verizon.net>
2017-12-28 21:28:19 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.collect do |day, supplies|
       if supplies == "BBQ"
-        puts array
+        puts day
       end
     end
   end
